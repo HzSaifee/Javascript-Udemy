f7cd1f649271f7d1f85c9215de908932fe25d23c
Huzefa Saifee <hzsaifee52@gmail.com>
2020-04-24 16:55:19 -0600

    Brushed up concepts of JS

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,1 +1,54 @@
+/*****************************
+**Variables and DataTypes**
+
+var firstName = 'John';
+var lastName = 'Snow';
+var number = 1;
+var trueHeir = true;
 console.log('Hello World!!!');
+*****************************/
+
+
+
+/*****************************
+ **Variables mutation and coercion**
+
+alert(firstName + ' ' + lastName);
+var query = prompt('What do you know Anyways?);
+console.log('What do you mean by ' + query);
+console.log('That is ' + trueHeir + ', You know nothing, ' + firstName + ' ' + lastName);
+*****************************/
+
+
+
+/*****************************
+* Basic operators
+*****************************/
+
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yeahMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older tha John');
+var x;
+console.log(typeof x);
